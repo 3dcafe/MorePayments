9244e446b4c9c867f59055fdf2f35ac4a4551b49
Latin Nikolay <gs3d@yandex.ru>
2022-06-10 15:48:13 +0300

    update test client

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World! Start pay:");
 Console.WriteLine("Enter login:");
 string loginYookassa = Console.ReadLine();
 Console.WriteLine("Enter Passoword");
